9e040691f39193aee25386cdae8274c7664b8bbc
Gap <puri.pongsomboonsuk@gmail.com>
2025-03-08 11:22:35 +0700

    Task2 create component and binding data

--- 713-frontend/src/views/EventListView.vue
+++ 713-frontend/src/views/EventListView.vue
@@ -1,5 +1,13 @@
-<script setup lang="ts"></script>
+
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue'
+</script>
+
 
 <template>
-  <div></div>
+  <div>
+    <EventCard />
+    <EventCard />
+    <EventCard />
+  </div>
 </template>
