7145a81edc82cff805246750968ad3efc3757deb
Gap <puri.pongsomboonsuk@gmail.com>
2025-03-08 13:43:41 +0700

    Task4 using props injection

--- 713-frontend/src/components/EventCard.vue
+++ 713-frontend/src/components/EventCard.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-withDefaults(defineProps<{ name: string }>(), {
-  name: 'default name',
-})
+import type { Event } from '@/types';
+
+defineProps<{ event: Event }>();
 </script>
 
 <template>
   <div class="event-card">
-    <h1>This is an {{ name }} page</h1>
+    <div class="event-card">
+      <h2>{{ event.title }}</h2>
+      <p>{{ event.time }} on {{ event.date }}</p>
+    </div>
   </div>
 </template>
 
--- 713-frontend/src/views/EventListView.vue
+++ 713-frontend/src/views/EventListView.vue
@@ -3,15 +3,59 @@
 import {ref} from 'vue'
 import EventCard from '@/components/EventCard.vue'
 const events = ref([
-  { id: 1, name: 'Event 1' },
-  { id: 2, name: 'Event 2' },
-  { id: 3, name: 'Event 3' },
+  {
+    id: 1,
+    category: 'Category 1',
+    title: 'Event 1',
+    description: 'Description of Event 1',
+    date: '2022-01-01',
+    time: '12:00',
+    location: 'Location 1',
+    petsAllowed: true,
+    organizer: {
+      name: 'Organizer 1',
+    }
+  },
+  {
+    id: 2,
+    category: 'Category 2',
+    title: 'Event 2',
+    description: 'Description of Event 2',
+    date: '2022-02-02',
+    time: '13:00',
+    location: 'Location 2',
+    petsAllowed: false,
+    organizer: {
+      name: 'Organizer 2',
+    }
+  },
+  {
+    id: 3,
+    category: 'Category 3',
+    title: 'Event 3',
+    description: 'Description of Event 3',
+    date: '2022-03-03',
+    time: '14:00',
+    location: 'Location 3',
+    petsAllowed: true,
+    organizer: {
+      name: 'Organizer 3',
+    }
+  }
 ])
 </script>
 
 
 <template>
-  <div>
-<EventCard v-for="event in events" :key="event.id" :name="event.name" />
+  <div class="events">
+<EventCard v-for="event in events" :key="event.id" :event="event" />
 </div>
 </template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
