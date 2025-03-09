222348eb0fbbb7405f349653c5e778f9c2216ace
gapdapper <puri.pongsomboonsuk@gmail.com>
2025-03-09 22:33:51 +0700

    Task10 add sub routing

---
+++ 713-frontend/src/views/event/LayoutView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Event } from '@/types'
+import eventService from '@/services/EventService'
+const event = ref<Event>()
+const props = defineProps<{ id: string }>()
+const id = Number(props.id)
+eventService
+  .getEvent(id)
+  .then((response) => {
+    event.value = response.data
+  })
+  .catch ((error) => {
+    console.error('There was an error!', error)
+  })
+</script>
+
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <nav>
+      <router-link :to="{ name: 'event-detail-view', params: { id }}">Details</router-link>
+      <router-link :to="{ name: 'event-register-view', params: { id } }">Register</router-link>
+      <router-link :to="{ name: 'event-edit-view', params: { id } }">Edit</router-link>
+    </nav>
+    <router-view :event="event"></router-view>
+  </div>
+  <div v-else>
+    <p>Loading...</p>
+  </div>
+</template>
+
+<style></style>
