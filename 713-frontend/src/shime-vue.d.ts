d22e94d6b578471789b8b42d046ae912c14a0e6e
Gap <puri.pongsomboonsuk@gmail.com>
2025-03-17 22:42:03 +0700

    Lab7 Task12 fix error on vue-icon

---
+++ 713-frontend/src/shime-vue.d.ts
@@ -0,0 +1,10 @@
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+declare module '@jamescoyle/vue-icon' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
