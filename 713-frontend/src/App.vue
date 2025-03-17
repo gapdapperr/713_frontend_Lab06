b69647e485e17247577f197fc6bac94de0c8a1b8
Gap <puri.pongsomboonsuk@gmail.com>
2025-03-17 22:25:36 +0700

    Lab7 Task10 add login menu

--- 713-frontend/src/App.vue
+++ 713-frontend/src/App.vue
@@ -1,49 +1,97 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import { useMessageStore } from '@/stores/message'
-import { storeToRefs } from 'pinia';
+import { storeToRefs } from 'pinia'
 const store = useMessageStore()
 const { message } = storeToRefs(store)
+import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiAccountPlus, mdiAccount } from '@mdi/js'
+import { mdiLogin, mdiLogout } from '@mdi/js'
+import { useAuthStore } from './stores/auth'
+import { useRouter } from 'vue-router'
+const authStore = useAuthStore()
+const router = useRouter()
+
+function logout() {
+  authStore.logout()
+  router.push({ name: 'login' })
+}
 </script>
 
 <template>
-<div id="layout">
-<header>
-  <div id="flashMessage" v-if="message">
-    <h4>{{ message }}</h4>
+  <div id="layout">
+    <header>
+      <div id="flashMessage" v-if="message">
+        <h4>{{ message }}</h4>
+      </div>
+      <nav class="flex">
+        <ul v-if="!authStore.currentUserName" class="flex navbar-nav ml-auto">
+          <li class="nav-item px-2">
+            <router-link to="/register" class="nav-link">
+              <div class="flex items-center">
+                <SvgIcon type="mdi" :path="mdiAccountPlus" /> <span class="ml-3">Sign Up</span>
+              </div>
+            </router-link>
+          </li>
+          <li class="nav-item px-2">
+            <router-link to="/login" class="nav-link">
+              <div class="flex items-center">
+                <SvgIcon type="mdi" :path="mdiLogin" /> <span class="ml-3">Login</span>
+              </div>
+            </router-link>
+          </li>
+        </ul>
+        <ul v-if="authStore.currentUserName" class="flex navbar-nav ml-auto">
+          <li class="nav-item px-2">
+            <router-link to="/profile" class="nav-link">
+              <div class="flex items-center">
+                <SvgIcon type="mdi" :path="mdiAccount" />
+                <span class="ml-3">{{ authStore.currentUserName }}</span>
+              </div>
+            </router-link>
+          </li>
+          <li class="nav-item px-2">
+            <a class="nav-link hover:cursor-pointer" @click="logout">
+              <div class="flex items-center">
+                <SvgIcon type="mdi" :path="mdiLogout" /> <span class="ml-3"> LogOut</span>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <div class="wrapper">
+        <nav>
+          <RouterLink :to="{ name: 'event-list-view' }">Event</RouterLink>
+          <RouterLink :to="{ name: 'about' }">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+    <RouterView />
   </div>
-  <div class="wrapper">
-    <nav>
-      <RouterLink :to="{ name: 'event-list-view'}">Event</RouterLink>
-      <RouterLink :to="{ name: 'about'}">About</RouterLink>
-    </nav>
-  </div>
-</header>
-<RouterView />
-</div>
 </template>
 
 <style>
 #layout {
- font-family: Avenir, Helvetica, Arial, sans-serif;
- -webkit-font-smoothing: antialiased;
- -moz-osx-font-smoothing: grayscale;
- text-align: center;
- color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 nav {
- padding: 30px;
+  padding: 30px;
 }
 nav a {
- font-weight: bold;
- color: #2c3e50;
+  font-weight: bold;
+  color: #2c3e50;
 }
 nav a.router-link-exact-active {
- color: #42b983;
+  color: #42b983;
 }
 
 h2 {
-  font-size: 20px
+  font-size: 20px;
 }
 
 @keyframes yellofade {
